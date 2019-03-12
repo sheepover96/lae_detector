8f35dac7c41f44da1faed4b99a08347dc4927632
sheepover96 <sheep.over@gmail.com>
2019-03-13 00:46:27 +0900

    test on real dataset

---
+++ README.md
@@ -0,0 +1,21 @@
+# Subaru-HSC: LAE detector
+
+## requirement
+- python3 or Docker
+
+## Required dataset format
+Dataset must be following CSV format.
+```csv:dataset format
+id, narrow_fits_data_path, g_fits_data_path
+```
+
+## How to use
+### python3
+1. 'python3 -m venv venv'
+1. 'source venv/bin/activate'
+1. 'pip install -r requirements.txt'
+1. 'python3 main.py dataset_path'
+
+### docker
+under construction
+
--- main.py
+++ main.py
@@ -1,4 +1,69 @@
+from keras import backend as K
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+from keras.models import load_model
+from keras.models import model_from_json
+from keras.optimizers import Adam
+from keras.preprocessing import image
+
+import pandas as pd
+import numpy as np
+import cv2
+import csv, argparse
+
+from utils.data_loader_multi import MultiBandDataLoader
+
+MODEL_PATH = 'model/cnn_model.json'
+WEIGHTS_PATH = 'model/cnn_weights.hdf5'
+
+parser = argparse.ArgumentParser(description='Subaru HSC: LAE detector')
+parser.add_argument('data_path', help='path to csv dataset')
+
+args = parser.parse_args()
+DATASET_PATH = args.data_path
+
+def predict(model, test_data):
+    test_data_np = np.array(test_data)
+    test_data_np = test_data_np.reshape(test_data_np.shape[0], 50, 50, 1)
+    #score = self.model.evaluate(test_data_np, test_label_np, verbose=0)
+    prob = model.predict(test_data_np)
+    pred = model.predict_classes(test_data_np)
+    return None, pred, prob
+
+def write_multiband_result(dataset, pred_narrow_result_list, pred_g_result_list, pred_multiband_result_list, prob_narrow_list, prob_g_list, output_path):
+    with open(output_path, 'w') as f:
+        writer = csv.writer(f)
+        for data_row, pred_narrow_result, pred_g_result, pred_multiband_result, prob_narrow, prob_g in zip(dataset, pred_narrow_result_list, pred_g_result_list, pred_multiband_result_list, prob_narrow_list, prob_g_list):
+            label = data_row[2]
+            image_number = data_row[4]
+            image_path1 = data_row[5]
+            image_path2 = data_row[6]
+            row = [image_number, image_path1, image_path2, label, pred_narrow_result, pred_g_result, pred_multiband_result, prob_narrow, prob_g]
+            writer.writerow(row)
 
 
-if __name__ == '__main__':
-    pass
+optimizer = Adam(lr=0.001)
+loss = 'categorical_crossentropy'
+cnn_metrics = ['accuracy']
+with open(MODEL_PATH, 'r') as f_model:
+    json_model = f_model.read()
+cnn_model = model_from_json(json_model)
+cnn_model.compile(loss=loss, optimizer=optimizer, metrics=cnn_metrics)
+cnn_model.load_weights(WEIGHTS_PATH)
+
+data_loader = MultiBandDataLoader(DATASET_PATH)
+dataset = data_loader.get_dataset()
+
+img_narrow = [data[0] for data in dataset]
+img_g = [data[1] for data in dataset]
+
+_, pred_narrow, prob_narrow = predict(cnn_model, img_narrow)
+_, pred_g, prob_g = predict(cnn_model, img_g)
+
+pred_multiband = []
+for narrow_pred, g_perd in zip(pred_narrow, pred_g):
+    pred = 1
+    if not( narrow_pred == 1 and g_perd==0):
+        pred = 0
+    pred_multiband.append(pred)
+
+write_multiband_result(dataset, pred_narrow, pred_g, pred_multiband, prob_narrow, prob_g, './result/multiband_test_classw_400_17.csv')
